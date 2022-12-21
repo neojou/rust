ff274e61cc4f8e865115cb09bae14ef9b0853493
neojou <neojou@gmail.com>
2022-12-21 20:16:11 +0800

    refine the example of security
    Signed-off-by: neojou <neojou@gmail.com>

--- security/src/main.rs
+++ security/src/main.rs
@@ -5,7 +5,16 @@ struct NotConfidential {
 } 
 
 fn main() {
+    let a: i32 = 1000;
+    let b: i8 = 1;
+    //// compiler error because of the type difference: i8 to i32
+    //// let c: i32 = a + b;
+    let c: i32 = a + (b as i32);
+    println!("{} + {} = {}", a, b, c);
+
     let u_value : u8 = 97;
+    //// compiler error because of the type difference: u8 to char
+    //// let a : char = u_value;
     let a : char = u_value as char;
     println!("a = {}", a);
 
